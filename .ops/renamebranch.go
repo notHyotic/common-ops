1f6a0a0ba329e0444e6d5c624c6b0b8f87e1188d
notHyotic <hy0t1c1@gmail.com>
2025-05-10 13:12:05 -0500

    add command to rename branch

---
+++ .ops/renamebranch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"lesiw.io/cmdio/sys"
+)
+
+func (Ops) Renamebranch() {
+	var rnr = sys.Runner().WithEnv(map[string]string{
+		"PWD": "./",
+	})
+	args := os.Args
+	newbranchname := args[2]
+	defer rnr.Close()
+
+	err := rnr.Run("git", "branch", "-M", newbranchname)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
