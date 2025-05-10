2cdf5de4b94a31d8522d9a81b0469488c26a81a5
notHyotic <hy0t1c1@gmail.com>
2025-05-10 13:05:52 -0500

    add example of how to import lib

---
+++ .ops/test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	dotnet "ops/libs/dotnet"
+
+	"lesiw.io/cmdio/sys"
+)
+
+// example of how to import libs from libs folder
+func (Ops) ImportExample() {
+	var rnr = sys.Runner().WithEnv(map[string]string{
+		"PWD": "./",
+	})
+
+	defer rnr.Close()
+
+	sln, err := dotnet.ParseSln("bn-api-microservices.sln")
+	for _, project := range sln {
+		log.Println(project.Path)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
